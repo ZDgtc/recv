ba9e8609007e798f75ad591c0429fa9f8b434bf2
zhuangda <605079324@qq.com>
2019-01-16 09:33:21 +0800

    修改token

--- conf/default.py
+++ conf/default.py
@@ -31,9 +31,9 @@ from django.conf.global_settings import *  # noqa
 # ==============================================================================
 # 在蓝鲸智云开发者中心 -> 点击应用ID -> 基本信息 中获取 APP_ID 和 APP_TOKEN 的值
 APP_ID = 'recv'
-APP_TOKEN = '13f36900-5a46-47e6-827a-8007de00c881'
+APP_TOKEN = '2d8ae9a3-fc3c-42a1-b178-698d84cc496e'
 # 蓝鲸智云开发者中心的域名，形如：http://paas.bking.com
-BK_PAAS_HOST = 'http://paas.blueking.com'
+BK_PAAS_HOST = 'http://paas.yun.cn:80'
 
 # 请求官方 API 默认版本号，可选值为："v2" 或 ""；其中，"v2"表示规范化API，""表示未规范化API
 DEFAULT_BK_API_VER = 'v2'
